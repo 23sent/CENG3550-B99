cea0f79db13beb9207fddc7ca9fd4d16c61171c9
23sent <buadalinmadi@gmail.com>
2021-03-04 22:48:47 +0300

    Implementations of basic functions.

--- CENG3550.sol
+++ CENG3550.sol
@@ -3,29 +3,61 @@ pragma solidity ^0.8.0;
 //ERC20 interface from https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/IERC20.sol
 import "./IERC20.sol";
 
-contract S23 is IERC20 {
+contract B99 is IERC20 {
+    
+    uint private total;
+    string public name;
+    string public symbol;
+
+    mapping (address => uint256) private balances;
+    mapping (address => mapping (address => uint256)) private allowanceMap;
+    
+    constructor () {
+        name = "B99";
+        symbol = "NINE";
+        total = 999999999;
+        balances[msg.sender] = total;
+        
+    }
     
     function totalSupply () public view override returns (uint256) {
-        return 0;
+        return total;
     }
     
     function balanceOf (address account) public view override returns (uint256) {
-        return 0;  
+        return balances[account];  
     } 
     
     function transfer (address recipient, uint256 amount) public override returns (bool) {
-        return true;
+       return transferFrom(msg.sender, recipient, amount);
     }
     
     function allowance (address owner, address spender) public view override returns (uint256) {
-        return 0;
+        return allowanceMap[owner][spender];
     }
     
     function approve(address spender, uint256 amount) public override returns (bool) {
+        allowanceMap[msg.sender][spender] = amount;
+        emit Approval(msg.sender, spender, amount);
         return true;
     }
 
     function transferFrom(address sender, address recipient, uint256 amount) public override returns (bool) {
+        require(balances[sender] < amount || amount > 0, "Please give me a reasonable transfer amount.");
+        if(msg.sender != sender){
+            require(allowanceMap[sender][msg.sender] >= amount, "Allowance falan filan");
+            allowanceMap[sender][msg.sender] -= amount;
+            //emit Approval(sender, msg.sender, amount);
+        }
+        
+        balances[sender] -= amount;
+        balances[recipient] += amount;
+        
+        emit Transfer(sender, recipient, amount);
+        
         return true;
     }
+ 
+    
+    
 }
