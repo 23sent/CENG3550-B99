e814aa6f4e9b65135392dccfcddb776c44d6f79b
23sent <buadalinmadi@gmail.com>
2021-03-04 22:56:05 +0300

    comments

--- CENG3550.sol
+++ CENG3550.sol
@@ -5,8 +5,8 @@ import "./IERC20.sol";
 
 contract B99 is IERC20 {
     
-    uint private total;
-    string public name;
+    uint private total;     // Total wealth of B99.
+    string public name; 
     string public symbol;
 
     mapping (address => uint256) private balances;
@@ -16,16 +16,16 @@ contract B99 is IERC20 {
         name = "B99";
         symbol = "NINE";
         total = 999999999;
-        balances[msg.sender] = total;
+        balances[msg.sender] = total; // I'm the creator and owner of everything. -for now
         
     }
-    
+
     function totalSupply () public view override returns (uint256) {
-        return total;
+        return total; //return all wealth. This value constant, because I can't implement any burn etc function that can change total.
     }
     
     function balanceOf (address account) public view override returns (uint256) {
-        return balances[account];  
+        return balances[account]; 
     } 
     
     function transfer (address recipient, uint256 amount) public override returns (bool) {
@@ -33,18 +33,20 @@ contract B99 is IERC20 {
     }
     
     function allowance (address owner, address spender) public view override returns (uint256) {
-        return allowanceMap[owner][spender];
+        return allowanceMap[owner][spender]; // Who can transfer from your account? And how much.
     }
     
     function approve(address spender, uint256 amount) public override returns (bool) {
-        allowanceMap[msg.sender][spender] = amount;
+        allowanceMap[msg.sender][spender] = amount; // Who can transfer from your account? And how much. Same thing but for set.
         emit Approval(msg.sender, spender, amount);
         return true;
     }
 
     function transferFrom(address sender, address recipient, uint256 amount) public override returns (bool) {
+        // Check if balance is enough for transfer.
         require(balances[sender] < amount || amount > 0, "Please give me a reasonable transfer amount.");
         if(msg.sender != sender){
+            //if it's not your account. Can you use it?
             require(allowanceMap[sender][msg.sender] >= amount, "Allowance falan filan");
             allowanceMap[sender][msg.sender] -= amount;
             //emit Approval(sender, msg.sender, amount);
